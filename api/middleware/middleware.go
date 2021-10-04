9ae00ef9d0c9848999dbb08d99bdf6f83b780cd4
Luigi Morel <morelmiles@gmail.com>
2021-10-04 17:44:54 +0300

    Add auth middleware

---
+++ api/middleware/middleware.go
@@ -0,0 +1,27 @@
+package middleware
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/morelmiles/school-mgt-system/api/auth"
+	"github.com/morelmiles/school-mgt-system/api/responses"
+)
+
+func SetMiddlewareJSON(next http.HandlerFunc) http.HandlerFunc {
+	return func (w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next(w,r)
+	} 
+}
+
+func SetMiddlewareAuthentication(next http.HandlerFunc) http.HandlerFunc {
+	return func (w http.ResponseWriter, r *http.Request) {
+		err := auth.ValidToken(r)
+		if err != nil {
+			responses.JSON(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+			return 	
+		}
+		next(w, r)
+	}
+}
