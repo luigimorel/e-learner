531f2e0a8d5140153fba6d82f3e8e2089ca2a629
Luigi Morel <morelmiles@gmail.com>
2021-10-04 17:49:49 +0300

    Set up server

---
+++ api/server.go
@@ -0,0 +1,36 @@
+package api
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/morelmiles/school-mgt-system/api/controllers"
+	"github.com/morelmiles/school-mgt-system/api/seed"
+)
+
+var server = controllers.Server{}
+
+func init() {
+	//loads values from .env
+	if err := godotenv.Load(); err != nil {
+		log.Println("Could not load env file😏")
+	}
+}
+
+func Run()  {
+	var err error
+
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error getting env file %v", err)
+	} else {
+		fmt.Println("We are getting the .env file 🎉🙈")
+	}
+		server.Init(os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
+
+		seed.Load(server.DB)
+
+		server.Run(":8080")
+}
