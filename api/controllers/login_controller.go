0cfddd844c0be7bdca413f93a313dc0f89c79507
Luigi Morel <morelmiles@gmail.com>
2021-10-04 17:42:46 +0300

    Add the login controller

---
+++ api/controllers/login_controller.go
@@ -0,0 +1,60 @@
+package controllers
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/morelmiles/school-mgt-system/api/auth"
+	"github.com/morelmiles/school-mgt-system/api/models"
+	"github.com/morelmiles/school-mgt-system/api/responses"
+	"github.com/morelmiles/school-mgt-system/api/utils"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func (server *Server) Login(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		responses.ERROR(w,http.StatusUnprocessableEntity, err)
+		return 
+	}
+
+	user := models.Student{}
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	user.Prepare()
+	err = user.Validate("login")
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return 
+	}
+	token, err := server.SignIn(user.Email, user.Password)
+	if err != nil {
+		formattedError := utils.FormatError(err.Error())
+		responses.ERROR(w, http.StatusUnprocessableEntity, formattedError)
+		return 
+	}
+	responses.JSON(w, http.StatusOK, token)
+}
+
+func (server *Server) SignIn(email, password string) (string, error )  {
+	var err error 
+
+	user := models.Student{}
+
+	err = server.DB.Debug().Model(models.Student{}).Where("email = ?", email).Take(&user).Error
+	if err != nil {
+		return "", err 
+	}
+
+	err = models.VerifyPassword(user.Password, password)
+	if err != nil && err == bcrypt.ErrMismatchedHashAndPassword {
+		return "", err 
+	}
+	return auth.CreateToken(user.ID)
+
+}
