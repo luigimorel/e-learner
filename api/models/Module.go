0f3b518f1a88b2d7a974ac08186b85156cc86cd7
Luigi Morel <morelmiles@gmail.com>
2021-10-05 10:22:24 +0300

    Format the module model

--- api/models/Module.go
+++ api/models/Module.go
@@ -10,15 +10,15 @@ import (
 )
 
 type Module struct {
-	ID        			uint32 				`gorm:"primary_key;auto_increment" json:"id"`
-	Content   		string 			`gorm:"text;not null" json:"module_content"`
-	Title 			string 					`gorm:"size 255;not null;" json:"title"`
-	Creator    		Tutor				  `json:"author"`
-	CreatorID  		uint32 			`gorm:"not null;" json:"author_id"`
-	MainCourse 	  Course 		`json:"main_course"`
-	MainCourseID  uint32 	`gorm:"not null;" json:"main_course_id"`
-	CreatedAt 		time.Time		`gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
-	UpdatedAt		time.Time	`gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+	ID        				uint32 				`gorm:"primary_key;auto_increment" json:"id"`
+	Content   			string 				`gorm:"text;not null" json:"module_content"`
+	Title 					string 					`gorm:"size 255;not null;" json:"title"`
+	Creator    			Tutor				  `json:"author"`
+	CreatorID  			uint32 				`gorm:"not null;" json:"author_id"`
+	MainCourse 		  Course 			`json:"main_course"`
+	MainCourseID 	 uint32 			`gorm:"not null;" json:"main_course_id"`
+	CreatedAt 			time.Time		`gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt			time.Time		`gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
 }
 
 func (module *Module) Prepare()  {
